864d6a0a3ce3d64f399977a77248a1f920507a9e
Dheerapat Tookkane <dheerapat.t@kkumail.com>
2023-05-06 00:19:29 +0700

    finish publication page

--- src/components/Publications.svelte
+++ src/components/Publications.svelte
@@ -5,12 +5,30 @@
 
 <section>
     <h1>Publications</h1>
-    {#each publicationDatas as data }
-    <PublicationCard
-    title = {data.title}
-    year = {data.year}
-    journal = {data.journal}
-    author = {data.authors}
-    />
-    {/each}
+    <div class="wrapper">
+        {#each publicationDatas as data }
+        <PublicationCard
+        title = {data.title}
+        year = {data.year}
+        journal = {data.journal}
+        author = {data.authors}
+        />
+        {/each}
+    </div>
 </section>
+
+<style>
+    section {
+        background-color: #f5f5f5;
+        text-align: center;
+        font-size: 30px;
+        padding: 30px;
+    }
+
+    .wrapper {
+        display: grid;
+        grid-template-columns:  1fr 1fr;
+        padding: 50px;
+        justify-content: center;
+    }
+</style>
--- src/components/profilecards/PublicationCard.svelte
+++ src/components/profilecards/PublicationCard.svelte
@@ -11,3 +11,20 @@
     <p>{year}</p>
     <h3>{author}</h3>
 </div>
+
+<style>
+    div {
+        text-align: left;
+        font-size: medium;
+        margin: 15px;
+        background-color: white;
+        padding: 15px;
+        border-radius: 15px;
+        border-style: dashed;
+        border-color: black;
+        border-width: 2px;
+    }
+    div > * {
+        margin: 10px;
+    }
+</style>
