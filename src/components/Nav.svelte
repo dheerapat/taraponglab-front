ae86fd9fea9e7870bad69994337b8095c436e1be
dheerapat <dheerapat.t@kkumail.com>
2023-05-24 14:09:06 +0700

    responsive nav bar

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,11 +1,16 @@
 <nav>
-    <a href="/"> <img src="/Banner-2.png" alt="QSARLabs logo" id="logo"> </a>
-    <a href="#about">About Us</a>
-    <a href="#researchers">Researchers</a>
-    <a href="#funding">Funding</a>
-    <a href="#publications">Publications</a>
-    <a href="#contact">Contact</a>
-    <a class="box" href="/">Models</a>
+    <a href="/"> <img src="/Banner-2.png" alt="QSARLabs logo" id="logo"></a>
+    <a class ="hidable" href="#about">About Us</a>
+    <a class ="hidable" href="#researchers">Researchers</a>
+    <a class ="hidable" href="#funding">Funding</a>
+    <a class ="hidable" href="#publications">Publications</a>
+    <a class ="hidable" href="#contact">Contact</a>
+    <a class="box hidable" href="/">Models</a>
+    <div>
+        <div class="toggle"></div>
+        <div class="toggle"></div>
+        <div class="toggle"></div>
+    </div>
 </nav>
 
 <style>
@@ -14,11 +19,11 @@
         justify-content: space-evenly;
         
         background: linear-gradient(45deg, rgb(26, 1, 117) 0%, rgba(225, 5, 34, 0) 70%) repeat scroll 0% 0%, linear-gradient(135deg, rgb(225, 5, 152) 10%, rgba(49, 5, 209, 0) 80%) repeat scroll 0% 0%, linear-gradient(225deg, hsla(179, 81%, 45%, 1) 10%, rgba(10, 219, 216, 0) 80%) repeat scroll 0% 0%, rgba(0, 0, 0, 0) linear-gradient(315deg, rgb(189, 5, 245) 100%, rgba(9, 245, 5, 0) 70%) repeat scroll 0% 0% ;
-        padding: 10px;
+        padding: 5px 20px;
         align-items: center;
-        position: fixed;
+        /* position: fixed;
         top: 0;
-        width: 100%;
+        width: 100%; */
         
     }
     
@@ -37,6 +42,28 @@
         padding: 5px 15px;
         border-radius: 10px;
         color: #143F6B;
+    }
+
+    .toggle {
+        width: 35px;
+        height: 5px;
+        background-color: white;
+        margin: 6px 0;
+    }
+
+    @media (max-width: 1200px) {
+        .hidable {
+            display: none;
+        }
+        nav {
+            justify-content: space-between;
+        }
+    }
+
+    @media (min-width: 1200px) {
+        .toggle {
+            display: none;
+        }
     }
 </style>
 
