2d3eb1875e5d6438fea1cf32ba34d6e9c3284501
dheerapat <dheerapat.t@kkumail.com>
2023-06-14 14:16:57 +0700

    show uploaded pic

--- src/components/UploadImage.svelte
+++ src/components/UploadImage.svelte
@@ -2,26 +2,27 @@
     let selectedImage;
     let uploadStatus = "";
     let result;
+    let imageURL;
 
     async function predict_spheroid() {
-        const image = selectedImage[0]
+        const image = selectedImage[0];
         const formData = new FormData();
         formData.append("file", image);
 
         try {
             const res = await fetch("/spheroiddeath/", {
                 method: "POST",
-                body: formData
+                body: formData,
             });
 
-            const resData = await res.json()
+            const resData = await res.json();
 
             if (resData.status == "complete") {
-                result = resData.result
-                console.log(result)
-            }
-            else {
-                alert("API Call Failed: Please report to admin")
+                result = resData.result;
+                imageURL = URL.createObjectURL(image);
+                console.log(result);
+            } else {
+                alert("API Call Failed: Please report to admin");
             }
         } catch (error) {
             uploadStatus = "An error occur while attempting upload image.";
@@ -30,17 +31,31 @@
 </script>
 
 <section>
-    <h1>PCAS-CNN : Spheroid cell death prediction using propidium iodide and calcein AM straining and convolutional neural network</h1>
-    <h4>You can upload spheroid images that have been stained with propidium iodide and calcein AM into this system. Our model will analyze the images and provide predictions for the live and dead cells within your spheroid.</h4>
-    <form on:submit|preventDefault={predict_spheroid} enctype="multipart/form-data" method="post">
+    <h1>
+        PCAS-CNN : Spheroid cell death prediction using propidium iodide and
+        calcein AM straining and convolutional neural network
+    </h1>
+    <h4>
+        You can upload spheroid images that have been stained with propidium
+        iodide and calcein AM into this system. Our model will analyze the
+        images and provide predictions for the live and dead cells within your
+        spheroid.
+    </h4>
+    <form
+        on:submit|preventDefault={predict_spheroid}
+        enctype="multipart/form-data"
+        method="post"
+    >
         <input type="file" accept="image/*" bind:files={selectedImage} />
         <button type="submit">Upload Image</button>
     </form>
     {#if result}
-    <br>
-    <h1>Result</h1>
-    <h4>Class: {result.class}</h4>
-    <h4>Probability: {result.prob.toPrecision(3)}%</h4>
+        <br />
+        <h1>Result</h1>
+        <!-- svelte-ignore a11y-img-redundant-alt -->
+        <img src={imageURL} alt="Uploaded Image" />
+        <h4>Class: {result.class}</h4>
+        <h4>Probability: {result.prob.toPrecision(3)}%</h4>
     {/if}
 </section>
 
@@ -57,6 +72,14 @@
     form {
         text-align: center;
     }
+
+    img {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 50%;
+    }
+
     form {
         margin-top: 100px;
     }
